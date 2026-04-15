a3cd719742d3c550247a3772c32f00454fbfbfb6
Yasmina <yasmina.cherkaoui@mail-esd.com>
2026-04-15 14:53:14 +0200

    feat: Zustand store and car/finish config

---
+++ src/store/useConfiguratorStore.js
@@ -0,0 +1,15 @@
+import { create } from 'zustand'
+
+export const useConfiguratorStore = create((set) => ({
+  activeCar: 'cybertruck',
+  setActiveCar: (id) => set({ activeCar: id, color: '#ffffff' }),
+
+  color: '#ffffff',
+  setColor: (color) => set({ color }),
+
+  finish: 'glossy',
+  setFinish: (finish) => set({ finish }),
+
+  shader: 'none',
+  setShader: (shader) => set({ shader }),
+}))
